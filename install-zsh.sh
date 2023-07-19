e5226cb7961c5dc7d44618c428e9118ccd4f76e6
Tiago <tiago.fernandes.godoy@gmail.com>
2023-07-19 10:44:52 -0300

    Updated README.MD and script installation.

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "\n[INF] Installing ZSH...\n"
-cd ~ && apt update && apt install -y zsh
+cd ~ && sudo apt update && sudo apt install -y zsh
 
 printf "\n[INF] Installing oh-my-zsh framework...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
@@ -10,15 +10,15 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 chsh -s $(which zsh)
 
 # init variable zsh_custom and zsh_config_file
-zsh_custom="~/.oh-my-zsh/custom"
-zsh_config_file=".zshrc"
+zsh_custom_plugins=$HOME/.oh-my-zsh/custom/plugins
+zsh_config_file=$HOME/.zshrc
 
 printf "\n[INF] Installing Autosuggestions and Syntax Highlighting plugins...\n"
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom_plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom_plugins/zsh-syntax-highlighting
 
 # activate the plugins in the .zshrc file
-sed -i 's/^plugins=(/plugins=(zsh-syntax-highlight zsh-autossuggestions /' $zsh_config_file
+sed -i 's/^plugins=(/plugins=(zsh-syntax-highlighting zsh-autosuggestions /' $zsh_config_file
 
 printf "\n[INF] Installing Starship theme...\n"
 curl -sS https://starship.rs/install.sh | sh
@@ -27,7 +27,7 @@ curl -sS https://starship.rs/install.sh | sh
 eval "$(starship init zsh)"
 
 printf "\n[INF] Installing firacode font...\n"
-apt install -y fonts-firacode
+sudo apt install -y fonts-firacode
 
 # spaceship theme in case you want to install it
 # cloning the spaceship theme
