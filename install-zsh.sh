50e215f3f61c87615ee7ac80894af9ecc3153579
Tiago <tiago.godoy@proton.me>
2023-07-18 22:16:25 -0300

    Added printf to log shell steps

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "\n[INF] Installing ZSH..."
+printf "\n[INF] Installing ZSH...\n"
 cd ~ && sudo apt update && sudo apt install -y zsh
 
-echo "\n[INF] Installing oh-my-zsh framework..."
+printf "\n[INF] Installing oh-my-zsh framework...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # set zsh as default shell
@@ -13,20 +13,20 @@ chsh -s $(which zsh)
 zsh_custom="~/.oh-my-zsh/custom"
 zsh_config_file=".zshrc"
 
-echo "\n[INF] Installing Autosuggestions and Syntax Highlighting plugins..."
+printf "\n[INF] Installing Autosuggestions and Syntax Highlighting plugins...\n"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $zsh_custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_custom/plugins/zsh-syntax-highlighting
 
 # activate the plugins in the .zshrc file
 sed -i 's/^plugins=(/plugins=(zsh-syntax-highlight zsh-autossuggestions /' $zsh_config_file
 
-echo "\n[INF] Installing Starship theme..."
+printf "\n[INF] Installing Starship theme...\n"
 curl -sS https://starship.rs/install.sh | sh
 
 # config shell to initialize starship
 eval "$(starship init zsh)"
 
-echo "\n[INF] Installing firacode font..."
+printf "\n[INF] Installing firacode font...\n"
 sudo apt install -y fonts-firacode
 
 # spaceship theme in case you want to install it
