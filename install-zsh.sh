3173e78edb375f998449220d22cc5b584b3f7fb1
Tiago Godoy <84431015+tiaggofg@users.noreply.github.com>
2024-08-26 23:45:33 -0300

    fix: adding starship init in the .zshrc file

--- install-zsh.sh
+++ install-zsh.sh
@@ -4,7 +4,7 @@ printf "\n[INF] Installing ZSH...\n"
 cd ~ && sudo apt update && sudo apt install -y zsh
 
 printf "\n[INF] Installing oh-my-zsh framework...\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""  
 
 # set zsh as default shell
 chsh -s $(which zsh)
@@ -24,17 +24,7 @@ printf "\n[INF] Installing Starship theme...\n"
 curl -sS https://starship.rs/install.sh | sh
 
 # config shell to initialize starship
-eval "$(starship init zsh)"
+echo eval "$(starship init zsh)" >> $zsh_config_file
 
 printf "\n[INF] Installing firacode font...\n"
 sudo apt install -y fonts-firacode
-
-# spaceship theme in case you want to install it
-# cloning the spaceship theme
-#git clone https://github.com/spaceship-prompt/spaceship-prompt.git $zsh_custom/themes/spaceship-prompt
-
-# cerating the symlink to the spaceship theme
-#ln -s $zsh_custom/themes/spaceship-prompt/spaceship.zsh-theme $zsh_custom/themes/spaceship.zsh-theme
-
-# activating the spacehip theme
-#sed -i 's/^ZSH_THEME=.*/ZSH_THEME="spaceship"/' ~/.zshrc
